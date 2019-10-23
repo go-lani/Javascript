13c12cb0efd0b17b4132edde7c5646abc96c885b
publizm <publizm@gmail.com>
2019-10-23 23:48:37 +0900

    배열 활용하기 03번 작업 완료

---
+++ array/03 copy.js
@@ -0,0 +1,46 @@
+// 프로퍼티 정렬
+// 요소의 프로퍼티(id, content, completed)를 문자열 인수로 전달하면 todos의 요소를 정렬하는 함수를 작성하라.
+// 단, todos는 변경되지 않도록 하자.
+
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+const todosCopy = todos.slice();
+
+function sortBy(key) {
+  function compare(key) {
+    return function (a, b) {
+      return a[key] > b[key] ? 1 : (a[key] < b[key] ? -1 : 0);
+    };
+  }
+
+  return todosCopy.sort(compare(key));
+}
+
+console.log(sortBy('id'));
+/*
+[
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false }
+]
+*/
+console.log(sortBy('content'));
+/*
+[
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false }
+]
+*/
+console.log(sortBy('completed'));
+/*
+[
+  { id: 3, content: 'HTML', completed: false },
+  { id: 1, content: 'Javascript', completed: false },
+  { id: 2, content: 'CSS', completed: true }
+]
+*/
