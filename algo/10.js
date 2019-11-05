150ef77dc409b9251931af93a654ee1f7711fc02
publizm <publizm@gmail.com>
2019-11-06 01:01:06 +0900

    docs: algorithm 10 complete

--- algo/10.js
+++ algo/10.js
@@ -1,18 +1,13 @@
-// 정수제곱근 판별하기
-// nextSqaure함수는 정수를 인수로 전달받는다.
-// n이 임의의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 임의의 정수 x의 제곱이 아니라면 ‘no’를 리턴하는 함수를 작성하라.
-
-function nextSqaure(n) {
-  const newNum = Math.sqrt(n);
-  if (Number.isInteger(newNum)) return Math.pow(newNum + 1, 2);
-  return 'no';
+// 배열의 최대/최소값 구하기
+// 배열의 요소 중 최대값/최소값을 반환하는 함수를 완성하라.
+function getMaxValueFromArray(array) {
+  const max = Math.max(...array);
+  return max;
 }
+console.log(getMaxValueFromArray([3, 6, -2, -5, 7, 3])); // 7
 
-console.log(nextSqaure());    // no
-console.log(nextSqaure(0));   // 1
-console.log(nextSqaure(1));   // 4
-console.log(nextSqaure(2));   // no
-console.log(nextSqaure(3));   // no
-console.log(nextSqaure(121)); // 144
-console.log(nextSqaure(165)); // no
-console.log(nextSqaure(400)); // 441
+function getMinValueFromArray(array) {
+  const min = Math.min(...array);
+  return min;
+}
+console.log(getMinValueFromArray([3, 6, -2, -5, 7, 3])); // -5
