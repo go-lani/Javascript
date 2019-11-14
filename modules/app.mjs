e0296bedeaa37262a75e6035a6bc92c38f510e1a
publizm <publizm@gmail.com>
2019-11-14 17:05:53 +0900

    ES 모듈 문법 테스트

---
+++ modules/app.mjs
@@ -0,0 +1,4 @@
+import { aa, bb } from './lib.mjs';
+
+aa(3);
+bb();
---
+++ modules/lib.mjs
@@ -0,0 +1,12 @@
+let a = 1;
+function aa(b) {
+  a = b;
+
+  console.log(a);
+};
+
+function bb() {
+  console.log('hello');
+};
+
+export { aa, bb }
