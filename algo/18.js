358bfbb8cb333c696c92887c93f79b819c47c412
publizm <publizm@gmail.com>
2019-11-27 22:38:42 +0900

    docs: modify algorithm 18

--- algo/18.js
+++ algo/18.js
@@ -4,10 +4,10 @@
 
 function generateRange(from, to) {
   const res = [];
-  for (let i = from; i <= 15; i++) {
+  for (let i = from; i <= to; i++) {
     res.push(i);
   }
   return res;
 }
 
-console.log(generateRange(10, 15)); // [ 10, 11, 12, 13, 14, 15 ]
+console.log(generateRange(10, 25)); // [ 10, 11, 12, 13, 14, 15 ]
