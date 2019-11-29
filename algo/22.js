c819a512d9cd7bd8aa625e2ccda8b36642140315
publizm <publizm@gmail.com>
2019-11-29 11:32:08 +0900

    docs: develop algorithm 22

--- algo/22.js
+++ algo/22.js
@@ -2,11 +2,7 @@
 // 배열을 인자로 전달받아 각 요소의 평균을 구하는 함수를 완성하라.
 
 function average(array) {
-  const sum = array.reduce((pre, cur) => {
-    return pre + cur;
-  }, 0);
-
-  return sum / array.length;
+  return array.reduce((pre, cur) => pre + cur, 0) / array.length;
 }
 
 console.log(average([5, 3, 4])); // 4
