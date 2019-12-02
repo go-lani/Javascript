eb0dd297eb0413d52c5e55b2305b758cbd1a53fc
publizm <publizm@gmail.com>
2019-12-02 19:58:36 +0900

    docs: modify repo

--- algo/21.js
+++
@@ -1,9 +0,0 @@
-// 배열에서 특정 값만을 구하기
-// 배열 arr에서 짝수이고 3보다 큰 수만을 구하여 이를 배열로 반환하는 함수를 작성하라
-
-function getArray(arr) {
-  const newArr = arr.filter(num => !(num % 2) && num > 3 ? num : false);
-  return newArr;
-}
-
-console.log(getArray([1, 2, 3, 4, 5, 6])); // [ 4, 6 ]
--- algo/22.js
+++
@@ -1,8 +0,0 @@
-// 평균 구하기
-// 배열을 인자로 전달받아 각 요소의 평균을 구하는 함수를 완성하라.
-
-function average(array) {
-  return array.reduce((pre, cur) => pre + cur, 0) / array.length;
-}
-
-console.log(average([5, 3, 4])); // 4
